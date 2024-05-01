b745b60cb4d671bbee86bf29e42ed8cbd67b1ce8
SamuelBarbosa1 <samuelbarbosa2001oliveira@gmail.com>
2024-05-01 17:33:30 -0300

    arrumando

--- app/(authenticate)/login.js
+++ app/(authenticate)/login.js
@@ -1,14 +1,153 @@
-import { StyleSheet, Text, View,SafeAreaView } from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  KeyboardAvoidingView,
+  TextInput,
+  Pressable,
+} from "react-native";
+import React, { useState } from "react";
+import { MaterialIcons } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
 
 const login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
   return (
-    <View>
-      <Text>login</Text>
-    </View>
-  )
-}
+    <SafeAreaView
+      style={{ flex: 1, backgroundColor: "#736bff", alignItems: "center" }}
+    >
+      <Text style={{ marginTop: 80 }}>
+        <Text style={{ fontSize: 18, fontWeight: "600", color: "white" }}>
+          LISTA DE TAREFAS
+        </Text>
+      </Text>
+      <KeyboardAvoidingView>
+        <View style={{ alignItems: "center" }}>
+          <Text
+            style={{
+              fontSize: 16,
+              fontWeight: "600",
+              marginTop: 20,
+              color: "black",
+            }}
+          >
+            Acesse com sua conta
+          </Text>
+        </View>
 
-export default login
+        <View style={{ marginTop: 70 }}>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              gap: 5,
+              backgroundColor: "white",
+              paddingVertical: 5,
+              borderRadius: 10,
+              marginTop: 30,
+            }}
+          >
+            <MaterialIcons
+              style={{ marginLeft: 8 }}
+              name="email"
+              size={24}
+              color="black"
+            />
+            <TextInput
+              value={email}
+              onChangeText={(text) => setEmail(text)}
+              style={{
+                color: "black",
+                marginVertical: 10,
+                width: 300,
+                fontSize: email ? 17 : 17,
+              }}
+              placeholder="digite seu email"
+            />
+          </View>
 
-const styles = StyleSheet.create({})
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              gap: 5,
+              backgroundColor: "white",
+              paddingVertical: 5,
+              borderRadius: 10,
+              marginTop: 30,
+            }}
+          >
+            <AntDesign
+              style={{ marginLeft: 8 }}
+              name="lock1"
+              size={24}
+              color="black"
+            />
+            <TextInput
+              value={password}
+              secureTextEntry={true}
+              onChangeText={(text) => setPassword(text)}
+              style={{
+                color: "black",
+                marginVertical: 10,
+                width: 300,
+                fontSize: email ? 17 : 17,
+              }}
+              placeholder="digite sua senha"
+            />
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              marginTop: 12,
+              justifyContent: "space-between",
+            }}
+          >
+            <Text>Mantanha-se conectado</Text>
+            <Text style={{ color: "white", fontWeight: "500" }}>
+              Esqueci minha senha
+            </Text>
+          </View>
+          <View style={{ marginTop: 60 }} />
+
+          <Pressable
+            style={{
+              width: 200,
+              backgroundColor: "white",
+              padding: 15,
+              borderRadius: 6,
+              marginLeft: "auto",
+              marginRight: "auto",
+            }}
+          >
+            <Text
+              style={{
+                textAlign: "center",
+                color: "black",
+                fontWeight: "bold",
+                fontSize: 16,
+              }}
+            >
+              Entrar
+            </Text>
+          </Pressable>
+
+          <Pressable onPress={() => router.replace("/register")} style={{ marginTop: 15 }}>
+            <Text style={{ textAlign: "center", fontSize: 15, color: "black" }}>
+              Não tem uma conta? Registre-se
+            </Text>
+          </Pressable>
+        </View>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  );
+};
+
+export default login;
+
+const styles = StyleSheet.create({});
--- app/(authenticate)/register.js
+++ app/(authenticate)/register.js
@@ -1,14 +1,141 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  KeyboardAvoidingView,
+  TextInput,
+  Pressable,
+} from "react-native";
+import React, { useState } from "react";
+import { MaterialIcons } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
 
 const register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
   return (
-    <View>
-      <Text>register</Text>
-    </View>
-  )
-}
+    <SafeAreaView
+      style={{ flex: 1, backgroundColor: "#736bff", alignItems: "center" }}
+    >
+      <Text style={{ marginTop: 80 }}>
+        <Text style={{ fontSize: 18, fontWeight: "600", color: "white" }}>
+          LISTA DE TAREFAS
+        </Text>
+      </Text>
+      <KeyboardAvoidingView>
+        <View style={{ alignItems: "center" }}>
+          <Text
+            style={{
+              fontSize: 16,
+              fontWeight: "600",
+              marginTop: 20,
+              color: "black",
+            }}
+          >
+            Cadatra-se uma conta
+          </Text>
+        </View>
 
-export default register
+        <View style={{ marginTop: 70 }}>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              gap: 5,
+              backgroundColor: "white",
+              paddingVertical: 5,
+              borderRadius: 10,
+              marginTop: 30,
+            }}
+          >
+            <MaterialIcons
+              style={{ marginLeft: 8 }}
+              name="email"
+              size={24}
+              color="black"
+            />
+            <TextInput
+              value={email}
+              onChangeText={(text) => setEmail(text)}
+              style={{
+                color: "black",
+                marginVertical: 10,
+                width: 300,
+                fontSize: email ? 17 : 17,
+              }}
+              placeholder="digite seu email"
+            />
+          </View>
 
-const styles = StyleSheet.create({})
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              gap: 5,
+              backgroundColor: "white",
+              paddingVertical: 5,
+              borderRadius: 10,
+              marginTop: 30,
+            }}
+          >
+            <AntDesign
+              style={{ marginLeft: 8 }}
+              name="lock1"
+              size={24}
+              color="black"
+            />
+            <TextInput
+              value={password}
+              secureTextEntry={true}
+              onChangeText={(text) => setPassword(text)}
+              style={{
+                color: "black",
+                marginVertical: 10,
+                width: 300,
+                fontSize: email ? 17 : 17,
+              }}
+              placeholder="digite sua senha"
+            />
+          </View>
+          
+          <View style={{ marginTop: 60 }} />
+
+          <Pressable
+            style={{
+              width: 200,
+              backgroundColor: "white",
+              padding: 15,
+              borderRadius: 6,
+              marginLeft: "auto",
+              marginRight: "auto",
+            }}
+          >
+            <Text
+              style={{
+                textAlign: "center",
+                color: "black",
+                fontWeight: "bold",
+                fontSize: 16,
+              }}
+            >
+              Cadastrar
+            </Text>
+          </Pressable>
+
+          <Pressable onPress={() => router.replace("/login")} style={{ marginTop: 15 }}>
+            <Text style={{ textAlign: "center", fontSize: 15, color: "black" }}>
+              Já tem uma nova conta? Registre-se
+            </Text>
+          </Pressable>
+        </View>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  );
+};
+
+export default register;
+
+const styles = StyleSheet.create({});
